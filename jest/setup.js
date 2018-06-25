a7033658aaf4d8afeeffa40cd6c64af2ac99f2e5
Sergei Garcia Ortega <sgarcia.dev@gmail.com>
2018-06-25 17:34:15 -0500

    remove selenium driver version

--- jest/setup.js
+++ jest/setup.js
@@ -9,13 +9,13 @@ function startSelenium () {
     return new Promise((resolve, reject) => {
         console.log('Installing selenium server ...'.bgBlue);
         selenium.install({
-            drivers: {
-                chrome: {
-                    version: '2.37',
-                    arch: process.arch,
-                    baseURL: 'https://chromedriver.storage.googleapis.com'
-                }
-            }
+            // drivers: {
+            //     chrome: {
+            //         version: '2.37',
+            //         arch: process.arch,
+            //         baseURL: 'https://chromedriver.storage.googleapis.com'
+            //     }
+            // }
         }, onSeleniumInstall);
 
         function onSeleniumInstall(err) {

8c1dfc241980844ed04425855d2906b5086aa1ac
Sergei Garcia Ortega <sgarcia.dev@gmail.com>
2018-06-26 11:03:36 -0500

    added async callback timeout

--- jest/test/google.spec.js
+++ jest/test/google.spec.js
@@ -16,12 +16,13 @@ beforeEach(async () => {
 })
 
 test('Google Search for WebdriverIO has correct title', async () => {
+    jest.setTimeout(30000)
     await client.url('https://www.google.com/ncr');
     await client.setValue('input[name=q]', 'WebdriverIO');
     await client.click('input[value="Google Search"]');
     const title = await client.getTitle();
     expect(title).toBe('WebdriverIO - Google Search');
-}, 25000);
+});
 
 afterEach(async () => {
     await client.end();
